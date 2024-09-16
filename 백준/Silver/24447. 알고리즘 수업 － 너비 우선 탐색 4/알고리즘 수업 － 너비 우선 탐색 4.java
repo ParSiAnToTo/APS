11e34d83a9bdcaed9ed6af13c2122dce393ac0c7
Choi <139411386+ParSiAnToTo@users.noreply.github.com>
2024-09-16 10:15:26 +0900

    [Silver II] Title: 알고리즘 수업 - 너비 우선 탐색 4, Time: 956 ms, Memory: 95796 KB -BaekjoonHub

---
+++ 백준/Silver/24447. 알고리즘 수업 － 너비 우선 탐색 4/알고리즘 수업 － 너비 우선 탐색 4.java
@@ -0,0 +1,89 @@
+import java.io.*;
+import java.util.*;
+import java.util.stream.IntStream;
+
+public class Main {
+
+    static boolean[] visited;
+    static ArrayList<Integer>[] graph;
+    static int[] visitedDepthList;
+    static int[] visitedList;
+
+    static void bfs(int start) {
+        Queue<Integer> q = new LinkedList<>();
+        int index = 1;
+        visited[start] = true;
+        visitedDepthList[start] = 0;
+        visitedList[start] = index++;
+        q.add(start);
+        int nowWidthCount = q.size();
+        int nextWidthCount = 0;
+        int depth = 1;
+
+        while (!q.isEmpty()) {
+            if (nowWidthCount == 0) {
+                depth++;
+                nowWidthCount = nextWidthCount;
+                nextWidthCount = 0;
+            }
+
+            int curr = q.poll();
+            nowWidthCount--;
+
+            for (int i : graph[curr]) {
+                if (!visited[i]) {
+                    visited[i] = true;
+                    visitedDepthList[i] = depth;
+                    visitedList[i] = index++;
+                    nextWidthCount++;
+                    q.add(i);
+                }
+            }
+        }
+    }
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        StringBuilder sb = new StringBuilder();
+
+        int N = Integer.parseInt(st.nextToken());
+        int M = Integer.parseInt(st.nextToken());
+        int R = Integer.parseInt(st.nextToken());
+
+        graph = new ArrayList[N + 1];
+        for (int i = 0; i < N + 1; i++) {
+            graph[i] = new ArrayList<>();
+        }
+
+        for (int i = 0; i < M; i++) {
+            st = new StringTokenizer(br.readLine());
+            int u = Integer.parseInt(st.nextToken());
+            int v = Integer.parseInt(st.nextToken());
+            graph[u].add(v);
+            graph[v].add(u);
+        }
+        for (int i = 0; i < N + 1; i++) {
+            graph[i].sort(Comparator.reverseOrder());
+        }
+
+        visited = new boolean[N + 1];
+        visitedList = new int[N + 1];
+        visitedDepthList = new int[N + 1];
+        Arrays.fill(visitedDepthList, -1);
+
+        bfs(R);
+
+        Long answer = IntStream.range(0, N + 1)
+                .mapToLong(i -> (long) visitedList[i] * visitedDepthList[i])
+                .sum();
+        sb.append(answer);
+
+        bw.write(sb.toString());
+        bw.flush();
+        bw.close();
+
+    } // end of main
+
+} // end of class
