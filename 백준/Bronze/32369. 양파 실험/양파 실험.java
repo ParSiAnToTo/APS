5b4820101e2c2460a8cf536190ecb79d794f075a
Choi <139411386+ParSiAnToTo@users.noreply.github.com>
2024-10-04 10:54:41 +0900

    [Bronze IV] Title: 양파 실험, Time: 68 ms, Memory: 11612 KB -BaekjoonHub

---
+++ 백준/Bronze/32369. 양파 실험/양파 실험.java
@@ -0,0 +1,40 @@
+import java.io.*;
+import java.util.StringTokenizer;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        StringBuilder sb = new StringBuilder();
+
+        int N = Integer.parseInt(st.nextToken());
+        int A = Integer.parseInt(st.nextToken());
+        int B = Integer.parseInt(st.nextToken());
+
+        int onionA = 1;
+        int onionB = 1;
+
+        for (int i = 0; i < N; i++) {
+            onionA += A;
+            onionB += B;
+
+            if (onionB > onionA) {
+                int temp = onionA;
+                onionA = onionB;
+                onionB = temp;
+            }
+
+            if (onionB == onionA) {
+                onionB--;
+            }
+        }
+
+        sb.append(onionA).append(" ").append(onionB);
+
+        bw.write(sb.toString());
+        bw.flush();
+        bw.close();
+    }
+}
