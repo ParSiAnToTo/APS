ba28903b5444cb8adff4218462f641bf383a6574
Choi <139411386+ParSiAnToTo@users.noreply.github.com>
2024-10-13 17:15:49 +0900

    [Bronze V] Title: 사랑은 고려대입니다, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/32384. 사랑은 고려대입니다/사랑은 고려대입니다.cc
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main(void) {
+	int N;
+	string str = "LoveisKoreaUniversity";
+	cin >> N;
+	for (int i = 0; i < N; i++)
+	{
+		cout << str << " ";
+	}	
+	cout << endl;
+
+	return 0;
+}
