d309057c04854967ab077841d5748ec0bb52e8e9
Choi <139411386+ParSiAnToTo@users.noreply.github.com>
2024-11-14 11:14:57 +0900

    [Silver V] Title: 재귀함수가 뭔가요?, Time: 68 ms, Memory: 11824 KB -BaekjoonHub

--- 백준/Silver/17478. 재귀함수가 뭔가요？/재귀함수가 뭔가요？.java
+++ 백준/Silver/17478. 재귀함수가 뭔가요？/재귀함수가 뭔가요？.java
@@ -1,30 +1,38 @@
-import java.util.Scanner;
+import java.io.*;
 
 public class Main {
-	public static int N;
 
-	public static void main(String[] args) {
-		Scanner sc = new Scanner(System.in);
-		N = sc.nextInt();
-		System.out.println("어느 한 컴퓨터공학과 학생이 유명한 교수님을 찾아가 물었다.");
-		recursion(0, "");
+    static StringBuilder sb;
+    static int N;
 
-	}// main
+    static void recursion(int depth, String s) {
+        if (depth == N) {
+            sb.append(s).append("\"재귀함수가 뭔가요?\"").append('\n');
+            sb.append(s).append("\"재귀함수는 자기 자신을 호출하는 함수라네\"").append('\n');
+            sb.append(s).append("라고 답변하였지.").append('\n');
+            return;
+        }
+        sb.append(s).append("\"재귀함수가 뭔가요?\"").append('\n');
+        sb.append(s).append("\"잘 들어보게. 옛날옛날 한 산 꼭대기에 이세상 모든 지식을 통달한 선인이 있었어.").append('\n');
+        sb.append(s).append("마을 사람들은 모두 그 선인에게 수많은 질문을 했고, 모두 지혜롭게 대답해 주었지.").append('\n');
+        sb.append(s).append("그의 답은 대부분 옳았다고 하네. 그런데 어느 날, 그 선인에게 한 선비가 찾아와서 물었어.\"").append('\n');
 
-	public static void recursion(int depth, String s) {
-		if (depth == N) {
-			System.out.println(s + "\"재귀함수가 뭔가요?\"");
-			System.out.println(s + "\"재귀함수는 자기 자신을 호출하는 함수라네\"");
-			System.out.println(s + "라고 답변하였지.");
-			return;
-		}
-		System.out.println(s + "\"재귀함수가 뭔가요?\"");
-		System.out.println(s + "\"잘 들어보게. 옛날옛날 한 산 꼭대기에 이세상 모든 지식을 통달한 선인이 있었어.");
-		System.out.println(s + "마을 사람들은 모두 그 선인에게 수많은 질문을 했고, 모두 지혜롭게 대답해 주었지.");
-		System.out.println(s + "그의 답은 대부분 옳았다고 하네. 그런데 어느 날, 그 선인에게 한 선비가 찾아와서 물었어.\"");
+        recursion(depth + 1, s + "____");
+        sb.append(s).append("라고 답변하였지.").append('\n');
+    }
 
-		recursion(depth + 1, s + "____");
-		System.out.println(s + "라고 답변하였지.");
-	}
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        sb = new StringBuilder();
 
-}// class
+        N = Integer.parseInt(br.readLine());
+
+        sb.append("어느 한 컴퓨터공학과 학생이 유명한 교수님을 찾아가 물었다.").append('\n');
+        recursion(0, "");
+
+        bw.write(sb.toString());
+        bw.flush();
+        bw.close();
+    }
+}
