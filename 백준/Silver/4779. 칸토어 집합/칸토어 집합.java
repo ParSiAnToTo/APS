5b8c9249ea10cd4963ddaf3563279515ef555eb3
Choi <139411386+ParSiAnToTo@users.noreply.github.com>
2024-11-14 10:44:55 +0900

    [Silver III] Title: 칸토어 집합, Time: 244 ms, Memory: 23728 KB -BaekjoonHub

--- 백준/Silver/4779. 칸토어 집합/칸토어 집합.java
+++ 백준/Silver/4779. 칸토어 집합/칸토어 집합.java
@@ -2,9 +2,7 @@ import java.io.*;
 
 public class Main {
 
-    static boolean[] line;
-
-    static void cantorSet(int st, int ed, int depth) {
+    static void cantorSet(int st, int ed, StringBuilder line, int depth) {
         if (depth == 0) {
             return;
         }
@@ -12,10 +10,10 @@ public class Main {
         int thirdSt = st + third;
         int thirdEd = thirdSt + third;
         for (int i = thirdSt; i < thirdEd; i++) {
-            line[i] = true;
+            line.setCharAt(i, ' ');
         }
-        cantorSet(st, thirdSt, depth - 1);
-        cantorSet(thirdEd, ed, depth - 1);
+        cantorSet(st, thirdSt, line, depth - 1);
+        cantorSet(thirdEd, ed, line, depth - 1);
     }
 
     public static void main(String[] args) throws IOException {
@@ -27,19 +25,9 @@ public class Main {
         while ((N = br.readLine()) != null && !N.isEmpty()) {
             int value = Integer.parseInt(N);
             int length = (int) Math.pow(3, value);
-            line = new boolean[length];
-
-            cantorSet(0, length, value);
-
-            for (int i = 0; i < line.length; i++) {
-                if (line[i]) {
-                    sb.append(" ");
-                } else {
-                    sb.append("-");
-                }
-            }
-
-            sb.append("\n");
+            StringBuilder line = new StringBuilder("-".repeat(length));
+            cantorSet(0, length, line, value);
+            sb.append(line).append("\n");
         }
 
         bw.write(sb.toString());
