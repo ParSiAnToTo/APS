fbd2d2758819f6dcb16af7e9959cce39f12b8308
Choi <139411386+ParSiAnToTo@users.noreply.github.com>
2024-11-16 09:41:10 +0900

    [Silver IV] Title: 덱, Time: 132 ms, Memory: 16544 KB -BaekjoonHub

--- 백준/Silver/10866. 덱/덱.java
+++ 백준/Silver/10866. 덱/덱.java
@@ -1,73 +1,72 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.ArrayDeque;
-import java.util.Deque;
-import java.util.StringTokenizer;
+import java.io.*;
+import java.util.*;
 
 public class Main {
 
-	public static void main(String[] args) throws NumberFormatException, IOException {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		int N = Integer.parseInt(br.readLine());
-		StringTokenizer st;
-		StringBuilder sb = new StringBuilder();
-		Deque<Integer> deque = new ArrayDeque<>();
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        StringBuilder sb = new StringBuilder();
 
-		for (int i = 1; i <= N; i++) {
-			st = new StringTokenizer(br.readLine());
-			switch (st.nextToken()) {
-			case "push_front":
-				deque.addFirst(Integer.parseInt(st.nextToken()));
-				break;
-			case "push_back":
-				deque.addLast(Integer.parseInt(st.nextToken()));
-				break;
-			case "pop_front":
-				if (deque.isEmpty()) {
-					sb.append(-1).append("\n");
-				} else {
-					sb.append(deque.pollFirst()).append("\n");
-				}
-				break;
-			case "pop_back":
-				if (deque.isEmpty()) {
-					sb.append(-1).append("\n");
-				} else {
-					sb.append(deque.pollLast()).append("\n");
-				}
-				break;
-			case "size":
-				sb.append(deque.size()).append("\n");
-				break;
-			case "empty":
-				if (deque.isEmpty()) {
-					sb.append(1).append("\n");
-				} else {
-					sb.append(0).append("\n");
-				}
-				break;
-			case "front":
-				if (deque.isEmpty()) {
-					sb.append(-1).append("\n");
-				} else {
-					sb.append(deque.peekFirst()).append("\n");
-				}
-				break;
-			case "back":
-				if (deque.isEmpty()) {
-					sb.append(-1).append("\n");
-				} else {
-					sb.append(deque.peekLast()).append("\n");
-				}
-				break;
-			default:
-				break;
-			}
+        int N = Integer.parseInt(st.nextToken());
 
-		}
-		br.close();
-		
-		System.out.println(sb.toString());
-	}
+        Deque<Integer> deque = new ArrayDeque<>();
+
+        for (int i = 0; i < N; i++) {
+            st = new StringTokenizer(br.readLine());
+            switch (st.nextToken()) {
+                case "push_front":
+                    deque.addFirst(Integer.parseInt(st.nextToken()));
+                    break;
+                case "push_back":
+                    deque.addLast(Integer.parseInt(st.nextToken()));
+                    break;
+                case "pop_front":
+                    if (deque.isEmpty()) {
+                        sb.append(-1).append("\n");
+                    } else {
+                        sb.append(deque.pollFirst()).append("\n");
+                    }
+                    break;
+                case "pop_back":
+                    if (deque.isEmpty()) {
+                        sb.append(-1).append("\n");
+                    } else {
+                        sb.append(deque.pollLast()).append("\n");
+                    }
+                    break;
+                case "size":
+                    sb.append(deque.size()).append("\n");
+                    break;
+                case "empty":
+                    if (deque.isEmpty()) {
+                        sb.append(1).append("\n");
+                    } else {
+                        sb.append(0).append("\n");
+                    }
+                    break;
+                case "front":
+                    if (deque.isEmpty()) {
+                        sb.append(-1).append("\n");
+                    } else {
+                        sb.append(deque.peekFirst()).append("\n");
+                    }
+                    break;
+                case "back":
+                    if (deque.isEmpty()) {
+                        sb.append(-1).append("\n");
+                    } else {
+                        sb.append(deque.peekLast()).append("\n");
+                    }
+                    break;
+                default:
+                    break;
+            }
+        }
+
+        bw.write(sb.toString());
+        bw.flush();
+        bw.close();
+    }
 }
