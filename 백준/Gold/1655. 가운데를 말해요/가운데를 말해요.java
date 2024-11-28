41f7faf136a911be42e35c85be60904bbbd7f25c
Choi <139411386+ParSiAnToTo@users.noreply.github.com>
2024-11-28 20:14:01 +0900

    [Gold II] Title: 가운데를 말해요, Time: 360 ms, Memory: 36720 KB -BaekjoonHub

--- 백준/Gold/1655. 가운데를 말해요/가운데를 말해요.java
+++ 백준/Gold/1655. 가운데를 말해요/가운데를 말해요.java
@@ -3,46 +3,41 @@ import java.io.BufferedWriter;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
+import java.util.Comparator;
 import java.util.PriorityQueue;
 
 public class Main {
 
-	public static void main(String[] args) throws IOException {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-		StringBuilder sb = new StringBuilder();
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringBuilder sb = new StringBuilder();
 
-		int N = Integer.parseInt(br.readLine());
-		PriorityQueue<Integer> left = new PriorityQueue<>((o1, o2) -> o2 - o1);
-		PriorityQueue<Integer> right = new PriorityQueue<>();
+        int N = Integer.parseInt(br.readLine());
+        PriorityQueue<Integer> left = new PriorityQueue<>(Comparator.reverseOrder());
+        PriorityQueue<Integer> right = new PriorityQueue<>();
 
-		int target, swap;
-		for (int i = 0; i < N; i++) {
-			target = Integer.parseInt(br.readLine());
-			if (left.size() == right.size()) {
-				left.offer(target);
-			} else {
-				right.offer(target);
-			}
+        int target, swap;
+        for (int i = 0; i < N; i++) {
+            target = Integer.parseInt(br.readLine());
+            if (left.size() == right.size()) {
+                left.offer(target);
+            } else {
+                right.offer(target);
+            }
 
-			if (!left.isEmpty() && !right.isEmpty()) {
-				if (right.peek() < left.peek()) {
-					swap = left.poll();
-					left.offer(right.poll());
-					right.offer(swap);
-				}
+            if (!left.isEmpty() && !right.isEmpty()) {
+                if (right.peek() < left.peek()) {
+                    swap = left.poll();
+                    left.offer(right.poll());
+                    right.offer(swap);
+                }
+            }
+            sb.append(left.peek()).append("\n");
+        }
 
-			}
-
-			sb.append(left.peek() + "\n");
-
-		}
-
-		bw.write(sb.toString());
-		bw.flush();
-		bw.close();
-		br.close();
-
-	} // end of main
-
-} // end of class
+        bw.write(sb.toString());
+        bw.flush();
+        bw.close();
+    }
+}
