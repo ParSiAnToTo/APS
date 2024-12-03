0388da5f1a12032a8bff52c8b6be49e0ce6b6e1f
Choi <139411386+ParSiAnToTo@users.noreply.github.com>
2024-12-03 10:23:32 +0900

    [Silver I] Title: 숨바꼭질, Time: 100 ms, Memory: 16344 KB -BaekjoonHub

--- 백준/Silver/1697. 숨바꼭질/숨바꼭질.java
+++ 백준/Silver/1697. 숨바꼭질/숨바꼭질.java
@@ -1,65 +1,56 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.ArrayList;
-import java.util.LinkedList;
-import java.util.Queue;
-import java.util.StringTokenizer;
+import java.io.*;
+import java.util.*;
 
 public class Main {
 
-	static int ans, count, K;
+    private static int findYou(int n, int m) {
+        Queue<Integer> q = new LinkedList<>();
+        boolean[] visited = new boolean[100001];
+        int time = 0;
+        q.add(n);
+        visited[n] = true;
+        if (n == m) {
+            return time;
+        }
 
-	public static void main(String[] args) throws IOException {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st = new StringTokenizer(br.readLine(), " ");
-		int N = Integer.parseInt(st.nextToken()); // 수빈이
-		K = Integer.parseInt(st.nextToken()); // 동생
-		
-		if(N==K) {
-			System.out.println(0);
-		} else {
-		bfs(N);
-		System.out.println(ans);
-		}
+        outer:
+        while (!q.isEmpty()) {
+            int size = q.size();
+            for (int i = 0; i < size; i++) {
+                int cur = q.poll();
+                if (cur == m) {
+                    break outer;
+                }
+                if (cur + 1 >= 0 && cur + 1 < 100001 && !visited[cur + 1]) {
+                    q.add(cur + 1);
+                    visited[cur + 1] = true;
+                }
+                if (cur - 1 >= 0 && cur - 1 < 100001 && !visited[cur - 1]) {
+                    q.add(cur - 1);
+                    visited[cur - 1] = true;
+                }
+                if (cur * 2 >= 0 && cur * 2 < 100001 && !visited[cur * 2]) {
+                    q.add(cur * 2);
+                    visited[cur * 2] = true;
+                }
+            }
+            time++;
+        }
+        return time;
+    }
 
-	}// end of main
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringTokenizer st = new StringTokenizer(br.readLine());
 
-	static void bfs(int st) {
-		Queue<Integer> q = new LinkedList<>();
-		boolean[] visited = new boolean[100001];
+        int N = Integer.parseInt(st.nextToken());
+        int M = Integer.parseInt(st.nextToken());
 
-		q.offer(st);
-		visited[st] = true;
+        int sec = findYou(N, M);
 
-		while (!q.isEmpty()) {
-			count++;
-			int size = q.size();
-			for (int i = 0; i < size; i++) {
-				int num = q.poll();
-				visited[num] = true;
-				if (num + 1 == K || num - 1 == K || num * 2 == K) {
-					ans = count;
-					return;
-				}
-
-				if (num - 1 >= 0 && !visited[num - 1]) {
-					visited[num - 1] = true;
-					q.offer(num - 1);
-				}
-				if (num + 1 <= 100000 && !visited[num + 1]) {
-					visited[num + 1] = true;
-					q.offer(num + 1);
-				}
-				if (num * 2 <= 100000 && !visited[num * 2]) {
-					visited[num * 2] = true;
-					q.offer(num * 2);
-				}
-
-			}
-
-		}
-
-	}
-
-}// end of class
+        bw.write(String.valueOf(sec));
+        bw.flush();
+        bw.close();
+    }
+}
