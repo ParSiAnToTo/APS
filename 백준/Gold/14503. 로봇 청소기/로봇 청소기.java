fb84910278e5ab7497c3fc632a973619aa011d67
Choi <139411386+ParSiAnToTo@users.noreply.github.com>
2024-12-05 21:12:27 +0900

    [Gold V] Title: 로봇 청소기, Time: 72 ms, Memory: 11952 KB -BaekjoonHub

--- 백준/Gold/14503. 로봇 청소기/로봇 청소기.java
+++ 백준/Gold/14503. 로봇 청소기/로봇 청소기.java
@@ -1,80 +1,78 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
+import java.io.*;
 import java.util.StringTokenizer;
 
 public class Main {
 
-	static int[] dr = { -1, 0, 1, 0 };
-	static int[] dc = { 0, 1, 0, -1 };
-	// 청소기의 전진방향 북 동 남 서
-	// 회전은 반시계방향이므로 북 ->서 ->남 ->동이다
-	
-	static int N, M, d, ans;
-	static int[][] clean;
-	static boolean[][] cleanChk;
+    static int N, M, cleanCount;
+    static int[][] room;
+    static boolean[][] cleaned;
+    static int[] dr = {-1, 0, 1, 0};
+    static int[] dc = {0, 1, 0, -1};
 
-	public static void main(String[] args) throws IOException {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st = new StringTokenizer(br.readLine(), " ");
-		N = Integer.parseInt(st.nextToken());
-		M = Integer.parseInt(st.nextToken());
-		st = new StringTokenizer(br.readLine(), " ");
-		int startR = Integer.parseInt(st.nextToken()); // 시작 행
-		int startC = Integer.parseInt(st.nextToken()); // 시작 열
-		d = Integer.parseInt(st.nextToken()); // 시작 방향
+    static void robotActivate(int startRow, int startCol, int startDir) {
+        while (true) {
+            if (!cleaned[startRow][startCol]) {
+                cleaned[startRow][startCol] = true;
+                cleanCount++;
+            }
 
-		clean = new int[N][M];
-		for (int i = 0; i < N; i++) {
-			st = new StringTokenizer(br.readLine(), " ");
-			for (int j = 0; j < M; j++) {
-				clean[i][j] = Integer.parseInt(st.nextToken());
-			}
-		}
+            boolean isMoved = false;
+            for (int i = 0; i < 4; i++) {
+                int nd = (startDir + 3) % 4;
+                int nr = startRow + dr[nd];
+                int nc = startCol + dc[nd];
+                if (nr >= 0 && nr < N && nc >= 0 && nc < M && room[nr][nc] == 0 && !cleaned[nr][nc]) {
+                    startRow = nr;
+                    startCol = nc;
+                    startDir = nd;
+                    isMoved = true;
+                    break;
+                }
+                startDir = nd;
+            }
 
-		cleanChk = new boolean[N][M];
+            if (!isMoved) {
+                int backDir = (startDir + 2) % 4;
+                int nr = startRow + dr[backDir];
+                int nc = startCol + dc[backDir];
+                if (nr >= 0 && nr < N && nc >= 0 && nc < M && room[nr][nc] == 0) {
+                    startRow = nr;
+                    startCol = nc;
+                } else {
+                    break;
+                }
+            }
 
-		ans = 0;
-		active(startR, startC, d);
+        }
+    }
 
-	}// end of main
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringTokenizer st = new StringTokenizer(br.readLine());
 
-	static void active(int r, int c, int dir) {
-		if (!cleanChk[r][c]) {
-			cleanChk[r][c] = true;
-			ans++;
-		} // 시작 칸은 반드시 비어있다. 청소 체크
-		boolean deltaChk = true;
-		for (int i = 0; i < 4; i++) { // delta 탐색
-			int nr = r + dr[i];
-			int nc = c + dc[i];
-			if (!cleanChk[nr][nc] && clean[nr][nc] == 0) { // 청소 안된 빈 칸이 존재
-				deltaChk = false;
-			}
-		}
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
 
-		if (deltaChk) { // 4칸 중 청소되지 않은 빈 칸이 존재하지 않는 경우
-			if (clean[r - dr[dir]][c - dc[dir]] == 0) { // 바라보는 방향으로 후진 가능 시
-				active(r - dr[dir], c - dc[dir], dir); // 후진하고 메서드 재실행
-			} else {
-				System.out.println(ans);
-				System.exit(0);
-			}
-		}
+        st = new StringTokenizer(br.readLine());
+        int startRow = Integer.parseInt(st.nextToken());
+        int startCol = Integer.parseInt(st.nextToken());
+        int startDir = Integer.parseInt(st.nextToken());
 
-		if (!deltaChk) { // 4칸 중 청소되지 않은 빈 칸 존재 시
-			for (int i =0; i <4; i++) {
-				dir = (dir + 3) % 4; // 반시계방향 90도 회전
-				int spinr = r + dr[dir];
-				int spinc = c + dc[dir];
-				if (!cleanChk[spinr][spinc] && clean[spinr][spinc] == 0) { // 바라보는 방향으로 전진한 칸이 청소 안된 빈 칸일 경우
-					active(spinr, spinc, dir);
-				}
+        room = new int[N][M];
+        cleaned = new boolean[N][M];
+        for (int i = 0; i < N; i++) {
+            st = new StringTokenizer(br.readLine());
+            for (int j = 0; j < M; j++) {
+                room[i][j] = Integer.parseInt(st.nextToken());
+            }
+        }
 
-			}
+        cleanCount = 0;
+        robotActivate(startRow, startCol, startDir);
 
-		}
-
-	}
-
-}// end of class
+        bw.write(String.valueOf(cleanCount));
+        bw.flush();
+        bw.close();
+    }
+}
