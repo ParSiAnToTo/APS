8b652dd786f0bf8776e4896054b0d13bbc2695e9
Choi <139411386+ParSiAnToTo@users.noreply.github.com>
2024-12-09 19:24:48 +0900

    [Gold IV] Title: 미로만들기, Time: 76 ms, Memory: 11912 KB -BaekjoonHub

--- 백준/Gold/2665. 미로만들기/미로만들기.java
+++ 백준/Gold/2665. 미로만들기/미로만들기.java
@@ -1,40 +1,35 @@
 import java.io.*;
-import java.util.*;
+import java.util.Arrays;
+import java.util.PriorityQueue;
 
 public class Main {
 
-    static int N;
-    static int[][] adjArr;
-    static int[][] dist;
-    static int[] dr = {-1, 0, 1, 0};
-    static int[] dc = {0, 1, 0, -1};
+    /*
+     * n x n 모양의 방
+     * 검은 방 1 흰방 0
+     * 검은 방은 입장 불가, 흰방끼리 통행 가능
+     * 0,0 -> n-1, n-1
+     * 이 루트로 갈 때 검은 방을 방문하는 최소 수는?
+     *
+     *
+     * */
+    static int[] dr = {-1, 1, 0, 0};
+    static int[] dc = {0, 0, -1, 1};
 
-    private static void dijk() {
-        PriorityQueue<int[]> pq = new PriorityQueue<>(Comparator.comparing(o -> o[2]));
-        pq.add(new int[]{0, 0, 0});
-        dist[0][0] = 0;
+    static class Node implements Comparable<Node> {
+        int x;
+        int y;
+        int count;
 
-        while (!pq.isEmpty()) {
-            int[] cur = pq.poll();
-            int curR = cur[0];
-            int curC = cur[1];
-            int curW = cur[2];
+        public Node(int x, int y, int count) {
+            this.x = x;
+            this.y = y;
+            this.count = count;
+        }
 
-            if (curR == N - 1 && curC == N - 1) {
-                break;
-            }
-
-            for (int i = 0; i < 4; i++) {
-                int nr = curR + dr[i];
-                int nc = curC + dc[i];
-                if (nr >= 0 && nc >= 0 && nr < N && nc < N) {
-                    int nw = curW + adjArr[nr][nc];
-                    if (nw < dist[nr][nc]) {
-                        dist[nr][nc] = nw;
-                        pq.add(new int[]{nr, nc, nw});
-                    }
-                }
-            }
+        @Override
+        public int compareTo(Node o) {
+            return this.count - o.count;
         }
     }
 
@@ -42,25 +37,54 @@ public class Main {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
-        N = Integer.parseInt(br.readLine());
-        adjArr = new int[N][N];
-
-        for (int i = 0; i < N; i++) {
+        int n = Integer.parseInt(br.readLine());
+        int[][] rooms = new int[n][n];
+        for (int i = 0; i < n; i++) {
             String line = br.readLine();
-            for (int j = 0; j < N; j++) {
-                int w = line.charAt(j) - '0';
-                adjArr[i][j] = w == 1 ? 0 : 1;
+            for (int j = 0; j < n; j++) {
+                rooms[i][j] = line.charAt(j) - '0';
             }
         }
 
-        dist = new int[N][N];
-        for (int i = 0; i < N; i++) {
+        int[][] dist = new int[n][n];
+        for (int i = 0; i < n; i++) {
             Arrays.fill(dist[i], Integer.MAX_VALUE);
         }
 
-        dijk();
+        PriorityQueue<Node> pq = new PriorityQueue<>();
+        pq.offer(new Node(0, 0, 0));
+        dist[0][0] = 0;
+        while (!pq.isEmpty()) {
+            Node cur = pq.poll();
 
-        bw.write(String.valueOf(dist[N - 1][N - 1]));
+            if (cur.x == -1 && cur.y == -1) {
+                break;
+            }
+
+            if (dist[cur.x][cur.y] < cur.count) {
+                continue;
+            }
+
+            for (int i = 0; i < 4; i++) {
+                int nextX = cur.x + dr[i];
+                int nextY = cur.y + dc[i];
+                if (nextX < 0 || nextX >= n || nextY < 0 || nextY >= n) {
+                    continue;
+                }
+                int nextCount = cur.count;
+                if (rooms[nextX][nextY] == 0) {
+                    nextCount++;
+                }
+                if (dist[nextX][nextY] > nextCount) {
+                    dist[nextX][nextY] = nextCount;
+                    pq.offer(new Node(nextX, nextY, nextCount));
+                }
+            }
+        }
+
+        int answer = dist[n-1][n-1];
+
+        bw.write(String.valueOf(answer));
         bw.flush();
         bw.close();
     }
