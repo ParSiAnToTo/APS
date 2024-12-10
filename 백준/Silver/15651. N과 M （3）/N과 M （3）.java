dbbcb5ded079cc309778f3d4e24eb965121a4e3a
Choi <139411386+ParSiAnToTo@users.noreply.github.com>
2024-12-10 23:02:56 +0900

    [Silver III] Title: N과 M (3), Time: 336 ms, Memory: 112248 KB -BaekjoonHub

--- 백준/Silver/15651. N과 M （3）/N과 M （3）.java
+++ 백준/Silver/15651. N과 M （3）/N과 M （3）.java
@@ -1,43 +1,45 @@
-import java.util.Scanner;
+import java.io.*;
+import java.util.StringTokenizer;
 
 public class Main {
-	public static int[] nums;
-	public static int[] result;
-	public static int N;
-	public static int M;
-	public static StringBuilder sb = new StringBuilder();
 
-	public static void main(String[] args) {
-		Scanner sc = new Scanner(System.in);
+    static int N, M;
+    static StringBuilder sb = new StringBuilder();
+    static int[] nums;
+    static int[] result;
 
-		N = sc.nextInt();
-		M = sc.nextInt();
+    static void combination(int depth) {
+        if (depth == M) {
+            for (int i = 0; i < M; i++) {
+                sb.append(result[i]).append(" ");
+            }
+            sb.append("\n");
+            return;
+        }
 
-		nums = new int[N];
-		for (int i = 1; i <= N; i++) {
-			nums[i - 1] = i;
-		}
-		result = new int[M];
+        for (int i = 1; i <= N; i++) {
+            result[depth] = nums[i];
+            combination(depth + 1);
+        }
+    }
 
-		dfs(N, M, 0);
-		System.out.println(sb);
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringTokenizer st = new StringTokenizer(br.readLine());
 
-	}
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
+        nums = new int[N + 1];
+        result = new int[M];
+        for (int i = 1; i <= N; i++) {
+            nums[i] = i;
+        }
 
-	public static void dfs(int n, int m, int depth) {
-		if (depth == M) {
-			for (int i = 0; i < depth; i++) {
-				sb.append(result[i]).append(" ");
-			}
-			sb.append("\n");
-			return;
-		}
+        combination(0);
 
-		for (int j = 0; j < n; j++) {
-			result[depth] = nums[j];
-			dfs(N, M, depth + 1);
-		}
-
-	} // dfs
-
+        bw.write(sb.toString());
+        bw.flush();
+        bw.close();
+    }
 }
