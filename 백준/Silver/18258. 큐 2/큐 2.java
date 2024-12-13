3f1188a1c75edad21fb65730344bb76c0f14e767
Choi <139411386+ParSiAnToTo@users.noreply.github.com>
2024-12-13 21:39:11 +0900

    [Silver IV] Title: 큐 2, Time: 916 ms, Memory: 321712 KB -BaekjoonHub

--- 백준/Silver/18258. 큐 2/큐 2.java
+++ 백준/Silver/18258. 큐 2/큐 2.java
@@ -1,66 +1,39 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.LinkedList;
-import java.util.Queue;
-import java.util.StringTokenizer;
+import java.io.*;
+import java.util.*;
 
 public class Main {
-	static int last;
 
-	public static void main(String[] args) throws IOException {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st = new StringTokenizer(br.readLine(), " ");
-		int N = Integer.parseInt(st.nextToken());
-		Queue<Integer> queue = new LinkedList<>();
-		StringBuilder sb = new StringBuilder();
-		
-		for (int i = 0; i < N; i++) {
-			st = new StringTokenizer(br.readLine(), " ");
-			String command = st.nextToken();
-			switch (command) {
-			case "push":
-				last = Integer.parseInt(st.nextToken());
-				queue.offer(last);
-				break;
-			case "pop":
-				if (!queue.isEmpty()) {
-					sb.append(queue.poll()).append("\n");
-				} else {
-					sb.append(-1).append("\n");
-				}
-				break;
-			case "size":
-				sb.append(queue.size()).append("\n");
-				break;
-			case "empty":
-				if (!queue.isEmpty()) {
-					sb.append(0).append("\n");
-				} else {
-					sb.append(1).append("\n");
-				}
-				break;
-			case "front":
-				if (!queue.isEmpty()) {
-					sb.append(queue.peek()).append("\n");
-				} else {
-					sb.append(-1).append("\n");
-				}
-				break;
-			case "back":
-				if (!queue.isEmpty()) {
-					sb.append(last).append("\n");
-				} else {
-					sb.append(-1).append("\n");
-				}
-				break;
-			default:
-				break;
-			}
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+        StringBuilder sb = new StringBuilder();
 
-		} //for
-		System.out.println(sb.toString());
+        int N = Integer.parseInt(st.nextToken());
+        Queue<Integer> q = new ArrayDeque<>();
+        int last = 0;
 
-	}// end of main
+        for (int i = 0; i < N; i++) {
+            st = new StringTokenizer(br.readLine(), " ");
+            String command = st.nextToken();
+            if (command.equals("push")) {
+                last = Integer.parseInt(st.nextToken());
+                q.add(last);
+            } else if (command.equals("pop")) {
+                sb.append(q.isEmpty() ? -1 : q.poll()).append('\n');
+            } else if (command.equals("size")) {
+                sb.append(q.size()).append('\n');
+            } else if (command.equals("empty")) {
+                sb.append(q.isEmpty() ? 1 : 0).append('\n');
+            } else if (command.equals("front")) {
+                sb.append(q.isEmpty() ? -1 : q.peek()).append('\n');
+            } else if (command.equals("back")) {
+                sb.append(q.isEmpty() ? -1 : last).append('\n');
+            }
+        }
 
-}// end of class
+        bw.write(sb.toString());
+        bw.flush();
+        bw.close();
+    }
+}
