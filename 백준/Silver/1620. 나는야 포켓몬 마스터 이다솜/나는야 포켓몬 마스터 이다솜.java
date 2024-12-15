3b4a1e0f5ef6afc51cb90ab16861968709c98db4
Choi <139411386+ParSiAnToTo@users.noreply.github.com>
2024-12-15 15:46:19 +0900

    [Silver IV] Title: 나는야 포켓몬 마스터 이다솜, Time: 448 ms, Memory: 61704 KB -BaekjoonHub

--- 백준/Silver/1620. 나는야 포켓몬 마스터 이다솜/나는야 포켓몬 마스터 이다솜.java
+++ 백준/Silver/1620. 나는야 포켓몬 마스터 이다솜/나는야 포켓몬 마스터 이다솜.java
@@ -1,49 +1,34 @@
-import java.io.BufferedReader;
-import java.io.BufferedWriter;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.io.OutputStreamWriter;
-import java.util.HashMap;
-import java.util.StringTokenizer;
+import java.io.*;
+import java.util.*;
 
 public class Main {
 
-	static boolean whatIsTheQuestion(String question) {
-		try {
-			Integer.parseInt(question);
-			return true;
-		} catch (Exception e) {
-			return false;
-		}
-	}
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        StringBuilder sb =new StringBuilder();
 
-	public static void main(String[] args) throws IOException {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-		StringTokenizer st = new StringTokenizer(br.readLine());
-		StringBuilder sb = new StringBuilder();
-		int N = Integer.parseInt(st.nextToken());
-		int M = Integer.parseInt(st.nextToken());
-		HashMap<Integer, String> mapK = new HashMap<>(N);
-		HashMap<String, Integer> mapV = new HashMap<>(N);
-		for (int i = 0; i < N; i++) {
-			String pokemon = br.readLine();
-			mapK.put(i + 1, pokemon);
-			mapV.put(pokemon, i + 1);
-		}
-		for (int i = 0; i < M; i++) {
-			String question = br.readLine();
-			if (whatIsTheQuestion(question)) {
-				sb.append(mapK.get(Integer.parseInt(question)) + "\n");
-			} else {
-				sb.append(mapV.get(question) + "\n");
-			}
-		}
-		bw.write(sb.toString());
-		bw.flush();
-		bw.close();
-		br.close();
+        int N = Integer.parseInt(st.nextToken());
+        int M = Integer.parseInt(st.nextToken());
+        HashMap<Integer, String> pokeDictIdx = new HashMap<>();
+        HashMap<String, Integer> pokeDictName = new HashMap<>();
+        for (int i = 1; i <= N; i++) {
+            String pokemon = br.readLine();
+            pokeDictIdx.put(i, pokemon);
+            pokeDictName.put(pokemon, i);
+        }
+        for (int i = 0; i < M; i++) {
+            String input = br.readLine();
+            if(input.charAt(0) - '0' >= 17){
+                sb.append(pokeDictName.get(input)).append("\n");
+            } else {
+                sb.append(pokeDictIdx.get(Integer.parseInt(input))).append("\n");
+            }
+        }
 
-	} // end of main
-
-} // end of class
+        bw.write(sb.toString());
+        bw.flush();
+        bw.close();
+    }
+}
