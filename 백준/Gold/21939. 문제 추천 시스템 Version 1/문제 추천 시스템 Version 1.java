23d127a1614aee8cc47bfd230e6a4d9f64363946
Choi <139411386+ParSiAnToTo@users.noreply.github.com>
2024-12-15 23:21:06 +0900

    [Gold IV] Title: 문제 추천 시스템 Version 1, Time: 1068 ms, Memory: 92024 KB -BaekjoonHub

--- 백준/Gold/21939. 문제 추천 시스템 Version 1/문제 추천 시스템 Version 1.java
+++ 백준/Gold/21939. 문제 추천 시스템 Version 1/문제 추천 시스템 Version 1.java
@@ -20,23 +20,23 @@ public class Main {
         StringBuilder sb = new StringBuilder();
 
         int N = Integer.parseInt(st.nextToken());
-        
+
         PriorityQueue<Problem> maxHeap = new PriorityQueue<>((p1, p2) -> {
             if (p1.difficulty != p2.difficulty) {
                 return Integer.compare(p2.difficulty, p1.difficulty);
             }
             return Integer.compare(p2.index, p1.index);
         });
-        
+
         PriorityQueue<Problem> minHeap = new PriorityQueue<>((p1, p2) -> {
             if (p1.difficulty != p2.difficulty) {
                 return Integer.compare(p1.difficulty, p2.difficulty);
             }
             return Integer.compare(p1.index, p2.index);
         });
-        
+
         Map<Integer, Problem> problemMap = new HashMap<>();
-        
+
         for (int i = 0; i < N; i++) {
             st = new StringTokenizer(br.readLine());
             int pIdx = Integer.parseInt(st.nextToken());
@@ -48,7 +48,7 @@ public class Main {
         }
 
         int M = Integer.parseInt(br.readLine());
-        
+
         for (int i = 0; i < M; i++) {
             st = new StringTokenizer(br.readLine());
             String command = st.nextToken();
@@ -56,13 +56,13 @@ public class Main {
             if (command.equals("add")) {
                 int pIdx = Integer.parseInt(st.nextToken());
                 int difficulty = Integer.parseInt(st.nextToken());
-                
+
                 if (problemMap.containsKey(pIdx)) {
                     Problem oldProblem = problemMap.get(pIdx);
                     maxHeap.remove(oldProblem);
                     minHeap.remove(oldProblem);
                 }
-                
+
                 Problem newProblem = new Problem(pIdx, difficulty);
                 problemMap.put(pIdx, newProblem);
                 maxHeap.offer(newProblem);
@@ -72,17 +72,19 @@ public class Main {
                 int direction = Integer.parseInt(st.nextToken());
 
                 if (direction == 1) {
-
                     while (!maxHeap.isEmpty() && !problemMap.containsKey(maxHeap.peek().index)) {
                         maxHeap.poll();
                     }
-                    sb.append(maxHeap.peek().index).append('\n');
-                } else {
 
+                    sb.append(maxHeap.peek().index).append('\n');
+
+                } else {
                     while (!minHeap.isEmpty() && !problemMap.containsKey(minHeap.peek().index)) {
                         minHeap.poll();
                     }
+
                     sb.append(minHeap.peek().index).append('\n');
+
                 }
 
             } else if (command.equals("solved")) {
@@ -90,9 +92,9 @@ public class Main {
 
                 if (problemMap.containsKey(solveIndex)) {
                     Problem solvedProblem = problemMap.get(solveIndex);
-                    problemMap.remove(solveIndex);
                     maxHeap.remove(solvedProblem);
                     minHeap.remove(solvedProblem);
+                    problemMap.remove(solveIndex);
                 }
             }
         }
