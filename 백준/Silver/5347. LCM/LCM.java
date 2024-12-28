c5f6f04a86514e6ea7639520eb20a6923e13b5fd
Choi <139411386+ParSiAnToTo@users.noreply.github.com>
2024-12-28 14:33:36 +0900

    [Silver V] Title: LCM, Time: 64 ms, Memory: 11516 KB -BaekjoonHub

---
+++ 백준/Silver/5347. LCM/LCM.java
@@ -0,0 +1,33 @@
+import java.io.*;
+import java.util.StringTokenizer;
+
+public class Main {
+
+    public static int GCD(int a, int b) {
+        if (b == 0) {
+            return a;
+        }
+        return GCD(b, a % b);
+    }
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        StringBuilder sb = new StringBuilder();
+
+        int n = Integer.parseInt(st.nextToken());
+
+        for (int i = 0; i < n; i++) {
+            st = new StringTokenizer(br.readLine());
+            int a = Integer.parseInt(st.nextToken());
+            int b = Integer.parseInt(st.nextToken());
+            int gcd = GCD(a, b);
+            sb.append((long) a * b / gcd).append("\n");
+        }
+
+        bw.write(sb.toString());
+        bw.flush();
+        bw.close();
+    }
+}
