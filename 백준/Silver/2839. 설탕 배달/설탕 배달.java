baba981e78e9c680ec107a2a9edb0b827c99fd57
Choi <139411386+ParSiAnToTo@users.noreply.github.com>
2025-01-23 09:52:24 +0900

    [Silver IV] Title: 설탕 배달, Time: 64 ms, Memory: 11628 KB -BaekjoonHub

--- 백준/Silver/2839. 설탕 배달/설탕 배달.java
+++ 백준/Silver/2839. 설탕 배달/설탕 배달.java
@@ -1,25 +1,28 @@
-import java.util.Scanner;
+import java.io.*;
+import java.util.Arrays;
 
 public class Main {
-	public static void main(String[] args) {
-		Scanner sc = new Scanner(System.in);
-		int N = sc.nextInt();
 
-		boolean chk = false;
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
-		search: for (int i = 0; i <= (N / 3); i++) {
-			for (int j = 0; j <= (N / 5); j++) {
-				if (i * 3 + j * 5 == N) {
-					chk = true;
-					System.out.println(j + i);
-					break search;
-				}
-			}
-		}
+        int N = Integer.parseInt(br.readLine());
+        int[] dp = new int[N + 1];
+        Arrays.fill(dp, Integer.MAX_VALUE);
+        dp[0] = 0;
 
-		if (!chk)
-			System.out.println(-1);
+        for (int i = 1; i <= N; i++) {
+            if (i >= 3 && dp[i - 3] != Integer.MAX_VALUE) {
+                dp[i] = Math.min(dp[i], dp[i - 3] + 1);
+            }
+            if (i >= 5 && dp[i - 5] != Integer.MAX_VALUE) {
+                dp[i] = Math.min(dp[i], dp[i - 5] + 1);
+            }
+        }
 
-	}
-
+        bw.write(dp[N] != Integer.MAX_VALUE ? String.valueOf(dp[N]) : "-1");
+        bw.flush();
+        bw.close();
+    }
 }
