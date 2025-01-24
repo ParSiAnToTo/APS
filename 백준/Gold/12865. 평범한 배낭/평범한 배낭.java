ca04b6b5f3725c3b57acdf1d87be7b8a459e6f63
Choi <139411386+ParSiAnToTo@users.noreply.github.com>
2025-01-24 12:25:56 +0900

    [Gold V] Title: 평범한 배낭, Time: 144 ms, Memory: 51564 KB -BaekjoonHub

--- 백준/Gold/12865. 평범한 배낭/평범한 배낭.java
+++ 백준/Gold/12865. 평범한 배낭/평범한 배낭.java
@@ -1,39 +1,38 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
+import java.io.*;
 import java.util.StringTokenizer;
 
 public class Main {
 
-	public static void main(String[] args) throws IOException {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st = new StringTokenizer(br.readLine(), " ");
-		int N = Integer.parseInt(st.nextToken());
-		int K = Integer.parseInt(st.nextToken());
-		int[] weights = new int[N + 1];
-		int[] values = new int[N + 1];
-		for (int i = 1; i <= N; i++) {
-			st = new StringTokenizer(br.readLine(), " ");
-			weights[i] = Integer.parseInt(st.nextToken());
-			values[i] = Integer.parseInt(st.nextToken());
-		}
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringTokenizer st = new StringTokenizer(br.readLine());
 
-		int[][] dp = new int[N + 1][K + 1];
+        int N = Integer.parseInt(st.nextToken());
+        int K = Integer.parseInt(st.nextToken());
 
-		for (int i = 1; i <= N; i++) {
-			for (int j = 1; j <= K; j++) {
-				if (weights[i] <= j) {
-					dp[i][j] = Math.max(dp[i - 1][j], dp[i - 1][j - weights[i]] + values[i]);
-				} else {
-					dp[i][j] = dp[i - 1][j];
-				}
+        int[][] item = new int[N + 1][2];
+        for (int i = 1; i <= N; i++) {
+            st = new StringTokenizer(br.readLine());
+            int weight = Integer.parseInt(st.nextToken());
+            int value = Integer.parseInt(st.nextToken());
+            item[i][0] = weight;
+            item[i][1] = value;
+        }
 
-			}
+        int[][] dp = new int[N + 1][K + 1];
+        for (int i = 1; i <= N; i++) {
+            for (int j = 1; j <= K; j++) {
+                if (item[i][0] <= j) {
+                    dp[i][j] = Math.max(dp[i - 1][j], dp[i - 1][j - item[i][0]] + item[i][1]);
+                } else {
+                    dp[i][j] = dp[i - 1][j];
+                }
+            }
+        }
 
-		}
-
-		System.out.println(dp[N][K]);
-
-	}// end of main
-
-}// end of class
+        bw.write(String.valueOf(dp[N][K]));
+        bw.flush();
+        bw.close();
+    }
+}
