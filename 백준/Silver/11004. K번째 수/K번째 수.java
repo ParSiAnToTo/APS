f3e4cedac72e2d4dc60ef27500a3e75fc1c7b047
Choi <139411386+ParSiAnToTo@users.noreply.github.com>
2025-04-07 15:44:10 +0900

    [Silver V] Title: K번째 수, Time: 4028 ms, Memory: 739380 KB -BaekjoonHub

---
+++ 백준/Silver/11004. K번째 수/K번째 수.java
@@ -0,0 +1,30 @@
+import java.io.*;
+import java.util.Collections;
+import java.util.LinkedList;
+import java.util.StringTokenizer;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int N = Integer.parseInt(st.nextToken());
+        int K = Integer.parseInt(st.nextToken());
+
+        LinkedList<Integer> list = new LinkedList<>();
+
+        st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < N; i++) {
+            list.add(Integer.parseInt(st.nextToken()));
+        }
+
+        Collections.sort(list);
+        int answer = list.get(K - 1);
+
+        bw.write(Integer.toString(answer));
+        bw.flush();
+        bw.close();
+    }
+}
